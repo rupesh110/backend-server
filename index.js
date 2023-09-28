566eaba349c6a0da360686a5a7048aed3aaff3d2
Rupesh <shrestharupesh110@gmail.com>
2023-09-29 02:19:43 +1000

    simple error

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
     res.send("Hello world, let's go Cloud");
 });
 
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+})
+
 const actualPort = process.env.PORT || PORT;
 
 app.listen(actualPort, '0.0.0.0', () => {
