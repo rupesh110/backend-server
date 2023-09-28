aa5b4add01a8e259656c6d61b3b233d55af5e61a
Rupesh <shrestharupesh110@gmail.com>
2023-09-29 02:09:32 +1000

    simple error

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express=require("express");
-const app=express();
-const PORT=4000;
+const express = require("express");
+const app = express();
+const PORT = 4000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello world, let's go Cloud");
-})
+});
 
-app.listen(process.env.PORT || PORT,()=>console.log(`Listening on ${PORT}`))
+const actualPort = process.env.PORT || PORT;
+
+app.listen(actualPort, '0.0.0.0', () => {
+    console.log(`Listening on ${actualPort}`);
+});
